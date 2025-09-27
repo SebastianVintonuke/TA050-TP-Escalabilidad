2adfeff14551ee943981f2229aef7d31be4d5d57
Sebastian <svintonuke@fi.uba.ar>
2025-09-27 19:17:21 -0300

    Merge

--- resultnode/main.py
+++ resultnode/main.py
@@ -2,18 +2,16 @@
 
 import logging
 import os
-import time
 from configparser import ConfigParser
-
-from common import QueryId
-from common.middleware.middleware import MessageMiddlewareQueue
-from common.middleware.tasks.result import ResultTask
 
 from middleware import routing
 from middleware.result_node_middleware import *
 from middleware.select_tasks_middleware import *
 from middleware.errors import *
 from middleware.routing.select_task_message import *
+from middleware.result_node_middleware import *
+from middleware.routing.csv_message import *
+from middleware.select_tasks_middleware import *
 
 
 def initialize_config():  # type: ignore[no-untyped-def]
@@ -63,122 +61,6 @@ def initialize_log(logging_level: int) -> None:
         datefmt="%Y-%m-%d %H:%M:%S",
     )
 
-def start_notifying(addr):
-    a_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    a_socket.connect(addr)
-
-    results_protocol = ResultsProtocol(a_socket)
-    fake_user_id = new_uuid()
-    partial_results_1 = [
-        QueryResult1.from_bytes(
-            "9af9901b-60a8-4f95-a586-980a725d1049,87.0".encode("utf-8")
-        ),
-        QueryResult1.from_bytes(
-            "6a58d026-823f-4bda-a15c-2c3f090b7a27,78.0".encode("utf-8")
-        ),
-    ]
-    partial_results_2 = [
-        QueryResult1.from_bytes(
-            "8845cdaa-d230-4453-bbdf-0e4f783045bf,76.5".encode("utf-8")
-        ),
-        QueryResult1.from_bytes(
-            "1b42e037-691b-40e1-9082-04743ef92f7b,75.0".encode("utf-8")
-        ),
-    ]
-    message_1 = ResultTask(
-        fake_user_id, QueryId.Query1, False, False, partial_results_1
-    ).to_bytes()
-    message_2 = ResultTask(
-        fake_user_id, QueryId.Query1, True, False, partial_results_2
-    ).to_bytes()
-    message_middleware_queue.send(message_1)
-    message_middleware_queue.send(message_2)
-    # CONSULTA 2MP
-    partial_results_3 = [
-        QueryResult2MostProfit.from_bytes(
-            "2025-01,8,3104830.0,Matcha Latte".encode("utf-8")
-        ),
-        QueryResult2MostProfit.from_bytes(
-            "2025-02,8,2804010.0,Matcha Latte".encode("utf-8")
-        ),
-    ]
-    partial_results_4 = [
-        QueryResult2MostProfit.from_bytes(
-            "2025-03,8,3103800.0,Matcha Latte".encode("utf-8")
-        ),
-        QueryResult2MostProfit.from_bytes(
-            "2025-04,8,2995990.0,Matcha Latte".encode("utf-8")
-        ),
-    ]
-    message_3 = ResultTask(
-        fake_user_id, QueryId.Query2MostProfit, False, False, partial_results_3
-    ).to_bytes()
-    message_4 = ResultTask(
-        fake_user_id, QueryId.Query2MostProfit, True, False, partial_results_4
-    ).to_bytes()
-    message_middleware_queue.send(message_3)
-    message_middleware_queue.send(message_4)
-    # CONSULTA 2BS
-    partial_results_5 = [
-        QueryResult2BestSelling.from_bytes(
-            "2025-01,5,156120,Flat White".encode("utf-8")
-        ),
-        QueryResult2BestSelling.from_bytes(
-            "2025-02,7,140290,Hot Chocolate".encode("utf-8")
-        ),
-        QueryResult2BestSelling.from_bytes(
-            "2025-03,2,155389,Americano".encode("utf-8")
-        ),
-        QueryResult2BestSelling.from_bytes("2025-04,6,150773,Mocha".encode("utf-8")),
-    ]
-    message_5 = ResultTask(
-        fake_user_id, QueryId.Query2BestSelling, True, False, partial_results_5
-    ).to_bytes()
-    message_middleware_queue.send(message_5)
-    # CONSULTA 3
-    partial_results_6 = [
-        QueryResult3.from_bytes(
-            "2025-H1,1,7967688.5,G Coffee @ USJ 89q".encode("utf-8")
-        ),
-        QueryResult3.from_bytes(
-            "2025-H1,2,8007511.0,G Coffee @ Kondominium Putra".encode("utf-8")
-        ),
-    ]
-    partial_results_7 = [
-        QueryResult3.from_bytes("2025-H1,9,7989533.5,G Coffee @ PJS8".encode("utf-8")),
-        QueryResult3.from_bytes(
-            "2025-H1,10,7968619.0,G Coffee @ Taman Damansara".encode("utf-8")
-        ),
-    ]
-    message_6 = ResultTask(
-        fake_user_id, QueryId.Query3, False, False, partial_results_6
-    ).to_bytes()
-    message_7 = ResultTask(
-        fake_user_id, QueryId.Query3, True, False, partial_results_7
-    ).to_bytes()
-    message_middleware_queue.send(message_6)
-    message_middleware_queue.send(message_7)
-    # CONSULTA 4
-    partial_results_8 = [
-        QueryResult4.from_bytes(
-            "5,1238919,5,G Coffee @ Seksyen 21,2001-04-10".encode("utf-8")
-        )
-    ]
-    partial_results_9 = [
-        QueryResult4.from_bytes(
-            "10,1196101,5,G Coffee @ Taman Damansara,1976-11-18".encode("utf-8")
-        )
-    ]
-    message_8 = ResultTask(
-        fake_user_id, QueryId.Query4, False, False, partial_results_8
-    ).to_bytes()
-    message_9 = ResultTask(
-        fake_user_id, QueryId.Query4, True, False, partial_results_9
-    ).to_bytes()
-    message_middleware_queue.send(message_8)
-    message_middleware_queue.send(message_9)
-    """
-
 def main() -> None:
     config_params = initialize_config()
     port = config_params["port"]
@@ -197,7 +79,7 @@ def main() -> None:
     result_middleware = ResultNodeMiddleware()
     tmp_send_middle = SelectTasksMiddleware()
 
-    msg_build = SelectTaskMessageBuilder(["8845cdaa-d230-4453-bbdf-0e4f783045bf,76.5"], ["query_1"])
+    msg_build = CSVMessageBuilder(["8845cdaa-d230-4453-bbdf-0e4f783045bf,76.5"], ["query_1"])
 
     ## From test
     rows_pass = [
