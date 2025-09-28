f8ceff973d229349b20a8b32261652275baea3c7
Sebastian <svintonuke@fi.uba.ar>
2025-09-28 00:24:48 -0300

    Fix QueryResults content

--- common/results/query2bs.py
+++ common/results/query2bs.py
@@ -7,9 +7,8 @@ from common.results.query import QueryResult
 @dataclass
 class QueryResult2BestSelling(QueryResult):
     year_month_created_at: date
-    item_id: int
-    sellings_qty: int
     item_name: str
+    sellings_qty: int
 
     @classmethod
     def from_bytes(cls, data: bytes) -> "QueryResult2BestSelling":
@@ -17,11 +16,10 @@ class QueryResult2BestSelling(QueryResult):
         fields = line.split(",")
 
         year_month_created_at = datetime.strptime(fields[0], "%Y-%m").date()
-        item_id = int(fields[1])
+        item_name = fields[1].strip()
         sellings_qty = int(fields[2])
-        item_name = fields[3].strip()
 
-        return cls(year_month_created_at=year_month_created_at, item_id=item_id, sellings_qty=sellings_qty, item_name=item_name)
+        return cls(year_month_created_at=year_month_created_at, sellings_qty=sellings_qty, item_name=item_name)
 
     def __str__(self) -> str:
-        return f"{self.year_month_created_at.strftime('%Y-%m')},{self.item_id},{self.sellings_qty},{self.item_name}"
+        return f"{self.year_month_created_at.strftime('%Y-%m')},{self.sellings_qty},{self.item_name}"
--- common/results/query3.py
+++ common/results/query3.py
@@ -1,29 +1,32 @@
 from datetime import datetime, date
 from dataclasses import dataclass
+from enum import Enum
 
 from common.results.query import QueryResult
 
+class HalfCreatedAt(str, Enum):
+    H1 = "H1"
+    H2 = "H2"
 
 @dataclass
 class QueryResult3(QueryResult):
     year_created_at: date
-    half_created_at: str
-    store_id: int
-    tpv: float
+    half_created_at: HalfCreatedAt
     store_name: str
+    tpv: float
 
     @classmethod
     def from_bytes(cls, data: bytes) -> "QueryResult3":
         line = data.decode("utf-8").strip()
         fields = line.split(",")
 
-        year_str, half_created_at = fields[0].split("-")
+        year_str, half_created_at_str = fields[0].split("-")
+        half_created_at = HalfCreatedAt(half_created_at_str)
         year_created_at = datetime.strptime(year_str, "%Y").date()
-        store_id = int(fields[1])
+        store_name = fields[1].strip()
         tpv = float(fields[2])
-        store_name = fields[3].strip()
 
-        return cls(year_created_at=year_created_at, half_created_at=half_created_at, store_id=store_id, tpv=tpv, store_name=store_name)
+        return cls(year_created_at=year_created_at, half_created_at=half_created_at, tpv=tpv, store_name=store_name)
 
     def __str__(self) -> str:
-        return f"{self.year_created_at.strftime('%Y')}-{self.half_created_at},{self.store_id},{self.tpv},{self.store_name}"
+        return f"{self.year_created_at.strftime('%Y')}-{self.half_created_at},{self.tpv},{self.store_name}"
