1bcfb766d7f8320d4376b93594381ebc77de153b
MarceloAgustinOrigoniGuillaume <marce.origoni@gmail.com>
2025-11-21 11:01:18 -0300

    refactored resultnode into a class and with logic of handlers in their files

---
+++ resultnode/src/handler_query3.py
@@ -0,0 +1,42 @@
+from common.middleware.tasks.result import ResultTask
+from common import QueryId
+
+from datetime import datetime, date
+from common.results.query3 import QueryResult3, HalfCreatedAt
+
+
+
+
+def __year_semester_decode(year_semester_str: str) -> Tuple[date, HalfCreatedAt]:
+    year_semester = int(year_semester_str)
+    year_str = str((year_semester * 6 // 12) + 2024)
+    year = datetime.strptime(str(year_str), "%Y").date()
+    if year_semester % 2 == 0:
+        semester = HalfCreatedAt.H1
+    else:
+        semester = HalfCreatedAt.H2
+    return year, semester
+
+class HandlerQuery3:
+    def handle_new_results(headers, msg,counter, user_id: str) -> bytes:
+        if headers.is_eof():
+            counter.expected_count_query_3 = headers.msg_count
+            logging.info(f"Received expected message count for query 3, expect {counter.expected_count_query_3} got: {counter.count_query_3}")
+
+            return
+        counter.count_query_3 += 1
+
+        data: List[QueryResult3] = []
+        for line in msg.stream_rows():
+            #logging.info(f"Q_3 {line}")
+            store_name = line[0]
+            year_created_at, half_created_at = __year_semester_decode(line[1])
+            tpv = float(line[2])
+            data.append(QueryResult3(year_created_at=year_created_at, half_created_at=half_created_at, store_name=store_name, tpv=tpv))
+        return ResultTask(user_id, QueryId.Query3, False, False, data).to_bytes()
+
+    def check_send_eof(counter, user_id, middleware):
+        if counter.is_eof_q3():
+            logging.info(f"Received last message for query 3 count: {counter.count_query_3} expected_count: {counter.expected_count_query_3}")
+            result_task = ResultTask(user_id, QueryId.Query3, True, False, []).to_bytes()
+            middleware.send(result_task)
